c001431d8906b413cc96fe8aa66af1126f73c4ad
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-01 09:44:46 -0300

    Update sequencial3.go

--- sequencial3.go
+++ sequencial3.go
@@ -1,5 +1,14 @@
 package main
 
 func main() {
-
+ var peso float64
+    var altura float64
+    fmt.Println("Digite seu peso ( em kg ): ")
+    fmt.Scan(&peso)
+    
+    fmt.Println("Digite sua altura ( em metros ): ")
+    fmt.Scan(&altura)
+    
+    fmt.Println("peso/(altura*altura)", peso/(altura*altura))
+    fmt.Println (" O seu imc eh de :",peso/(altura*altura) )
 }
