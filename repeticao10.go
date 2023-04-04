884c7f7173b7cc39235278aeaa84f52feb39a218
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-04 13:00:50 -0300

    Update repeticao10.go

--- repeticao10.go
+++ repeticao10.go
@@ -1,5 +1,23 @@
 package main
+//Faça um algoritmo que leia vários números inteiros e mostre o maior deles. 
+//A leitura deve ser interrompida quando for lido o valor zero.
 
-func main() {
+import "fmt"
 
+func main (){
+   var x, maior int
+    for {
+        fmt.Println("Digite um número inteiro (0 para sair): ")
+        fmt.Scan(&x)
+          
+          if x == 0{
+              break
+          }
+          
+          if x > maior {
+              maior = x
+          }
+    }
+    
+    fmt.Println("O maior número digitado foi :", maior)
 }
