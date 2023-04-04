28d1f25e6feb31a6c4c9424fdc689943c2213034
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-04 13:05:00 -0300

    Update selecao7.go

--- selecao7.go
+++ selecao7.go
@@ -1,5 +1,18 @@
 package main
+//Faça um algoritmo que leia o salário de um funcionário
+//e calcule o seu novo salário com um aumento de 10% se o salário for menor que R$ 1000,00;
+//ou de 5% se o salário for maior ou igual a R$ 1000,00.
+
+import "fmt"
 
 func main() {
-
+    var salário float64
+    fmt.Println("Digite o salário do funcionário:")
+    fmt.Scan(&salário)
+    
+    if salário < 1000 {
+        fmt.Println("Com o aumento o salário agora eh de: ",(salário*0.1) + salário )
+    } else if salário > 1000 {
+        fmt.Println("Com o aumento o salário agora eh  de:",(salário*0.05) + salário )
+    }
 }
