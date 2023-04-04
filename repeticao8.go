d34729db61d2dbb7f73a73895ed843c55e1bdb25
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-04 13:01:28 -0300

    Update repeticao8.go

--- repeticao8.go
+++ repeticao8.go
@@ -1,5 +1,18 @@
 package main
+//Faça um algoritmo que leia um número inteiro positivo e mostre todos os seus divisores.
 
-func main() {
+import "fmt"
 
+func main (){
+    var x int 
+    fmt.Println("Digite um número:")
+    fmt.Scan(&x)
+    
+    fmt.Println("os divisores de x são:", x)
+    
+    for i := 1; i <= x; i++{
+        if x % i == 0{
+            fmt.Println(i)
+        }
+    }
 }
