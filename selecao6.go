fcfc179f8319127dfe59aa0e8d537c03addedcc6
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-04 13:04:34 -0300

    Update selecao6.go

--- selecao6.go
+++ selecao6.go
@@ -1,5 +1,21 @@
 package main
+//Faça um algoritmo que leia dois números inteiros 
+//e mostre o resultado da multiplicação entre eles,
+//se ambos forem positivos; ou a soma, se pelo menos um deles for negativo.
+
+import "fmt"
 
 func main() {
-
+    var x, y int
+    fmt.Println("Digite um numero para x:")
+    fmt.Scan(&x)
+    
+    fmt.Println("Digite um numero para y:")
+    fmt.Scan(&y)
+    
+    if x > 0 && y > 0{
+        fmt.Println("os dois são positivos --> x*y = ", x*y)
+    } else if x < 0 || y < 0 {
+        fmt.Println("pelo menos um deles é negativo --> x+y = ",x+y)
+    }
 }
