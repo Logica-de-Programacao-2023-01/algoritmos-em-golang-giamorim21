596612ce3a4b958c49fb0514cac66862204c3710
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-04 13:01:55 -0300

    Update repeticao9.go

--- repeticao9.go
+++ repeticao9.go
@@ -1,5 +1,31 @@
 package main
+//Faça um algoritmo que leia vários números inteiros
+//e mostre a média aritmética entre eles.
+//A leitura deve ser interrompida quando for lido o valor zero.
 
-func main() {
+import "fmt"
 
+func main (){
+    var soma, quantidade, x int
+    
+    fmt.Println("Insira um número: ")
+    fmt.Scan(&x)
+    
+    if x != 0 {
+        soma += x
+        quantidade++
+    }
+    
+    for x != 0 {
+        fmt.Println("Insira um número: ")
+        fmt.Scan(&x)
+        
+        if x != 0 {
+            soma += x 
+            quantidade++
+        }
+    }
+    
+    média := soma / quantidade 
+    fmt.Println("A média eh", média)
 }
