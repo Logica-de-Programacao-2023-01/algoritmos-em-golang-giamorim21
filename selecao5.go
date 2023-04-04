45cbbeb2980b828a7606b48e6a8fda89e03f62e9
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-04 13:04:08 -0300

    Update selecao5.go

--- selecao5.go
+++ selecao5.go
@@ -1,5 +1,17 @@
 package main
+//Faça um algoritmo que leia um número inteiro 
+//e mostre se ele é múltiplo de 3 e de 5 ao mesmo tempo.
+
+import "fmt"
 
 func main() {
-
+    var x int 
+    fmt.Println("Digite um número:")
+    fmt.Scan(&x)
+    
+    if x % 3 == 0 && x % 5 == 0 {
+        fmt.Println("Esse número eh múltiplo de 3 e de 5 simultaneamente")
+    } else {
+        fmt.Println ("Esse número não eh múltiplo de 3 e de 5 simultaneamente")
+    }
 }
