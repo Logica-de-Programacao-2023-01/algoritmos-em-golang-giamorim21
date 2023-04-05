029dbd4f54e072f518813dd2d86b64cb090327ce
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:48:04 -0300

    Update sequencial7.go

--- sequencial7.go
+++ sequencial7.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+//Faça um algoritmo que leia um número inteiro
+//e mostre o seu sucessor e antecessor.
 
+import "fmt"
+
+func main() {
+    var x int
+
+    fmt.Print("Digite um número inteiro: ")
+    fmt.Scanln(&x)
+
+    antecessor := x - 1
+    sucessor := x + 1
+
+    fmt.Printfn("O antecessor de %d é %d e o sucessor é %d", x , antecessor, sucessor)
 }
