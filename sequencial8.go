fdcbfb252749ff24de17ba7d366a2bb4fa3d7bc3
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:48:26 -0300

    Update sequencial8.go

--- sequencial8.go
+++ sequencial8.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+//Faça um algoritmo que leia o número de dias trabalhados por um funcionário
+//e o valor da sua diária e calcule o seu salário.
 
+import "fmt"
+
+func main() {
+     var dias, diaria int
+     fmt.Println("Digite o numero de dias trabalhados (em dias) :")
+     fmt.Scan(&dias)
+     
+     fmt.Println("Digite o valor da sua diaria (em reais):")
+     fmt.Scan(&diaria)
+     
+     novoSalario := dias*diaria
+     
+     fmt.Println("O valor do novo salario do funcionario eh =", novoSalario ,"reais")
 }
