e998d0eb71f2d1747a42fc1f589e352d0c086ad2
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:47:42 -0300

    Update sequencial6.go

--- sequencial6.go
+++ sequencial6.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+//Faça um algoritmo que leia o salário de um funcionário 
+//e calcule o seu novo salário com um aumento de 15%.
 
+import "fmt"
+
+func main() {
+   var salario float64
+   fmt.Println("Digite qual eh o seu salario antigo:")
+   fmt.Scan(&salario)
+   
+   novoSalario := (salario * 0.15) + salario
+   
+   fmt.Println("O novo salário do funcionário é ", novoSalario)
+    
 }
