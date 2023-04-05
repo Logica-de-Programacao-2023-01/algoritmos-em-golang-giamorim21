4c8559c07272ba7945abed3ea2500a26cb51f7e5
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:45:49 -0300

    Update sequencial2.go

--- sequencial2.go
+++ sequencial2.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+//Faça um algoritmo que leia um número inteiro
+//e mostre o seu dobro, triplo e quádruplo.
 
+import "fmt"
+
+func main() {
+    var x int
+    
+    fmt.Println("Digite um numero para x:")
+    fmt.Scan(&x)
+    
+    fmt.Println("x*2 =", x*2)
+    fmt.Println("x*3 =", x*3)
+    fmt.Println("x*4 =", x*4)
 }
+
