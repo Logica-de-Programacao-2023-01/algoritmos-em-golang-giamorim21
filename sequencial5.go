9814deeaefa5239e97afc9765cd1dec70f500bbd
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:47:19 -0300

    Update sequencial5.go

--- sequencial5.go
+++ sequencial5.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+// Faça um algoritmo que leia a idade de uma pessoa em anos 
+//e mostre a idade em dias.   
 
+import "fmt"
+
+func main() {
+   var idade int
+   fmt.Println("Digite a sua idade (em anos):")
+   fmt.Scan(&idade)
+   
+   fmt.Println("Sua idade em dias =", idade*365)
 }
