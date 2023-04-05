5988d93cfae7c8bef8eadead88c6d11f1efcc8ce
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:46:38 -0300

    Update sequencial4.go

--- sequencial4.go
+++ sequencial4.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+// Faça um algoritmo que leia três números reais 
+//e mostre a média ponderada entre eles, com pesos 2, 3 e 5, respectivamente.
 
+import "fmt"
+
+func main() {
+[14:16, 31/03/2023] giovana amorim: var num1, num2, num3 float64
+
+    fmt.Print("Digite o primeiro número: ")
+    fmt.Scanln(&num1)
+    fmt.Print("Digite o segundo número: ")
+    fmt.Scanln(&num2)
+    fmt.Print("Digite o terceiro número: ")
+    fmt.Scanln(&num3)
+
+    fmt.Println("(num1*2 + num2*3 + num3*5)/10",(num1*2 + num2*3 + num3*5)/10)
+    
+    fmt.Println("A média ponderada é: ",(num1*2 + num2*3 + num3*5)/10 )
 }
