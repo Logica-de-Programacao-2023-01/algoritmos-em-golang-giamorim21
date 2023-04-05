f70694d8b24e77ffcbb94393a1540b9b9453257f
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:44:40 -0300

    Update selecao10.go

--- selecao10.go
+++ selecao10.go
@@ -1,5 +1,27 @@
 package main
+//Faça um algoritmo que leia a idade de uma pessoa 
+//e mostre a sua classificação, de acordo com a seguinte tabela:
+//até 9 anos: mirim
+//10 a 13 anos: infantil
+//14 a 17 anos: juvenil
+//maiores de 18 anos: adulto
 
-func main() {
+import "fmt"
 
+func main (){
+    var idade int 
+    fmt.Println("Digite a sua idade:")
+    fmt.Scan(&idade)
+    
+    switch {
+    case idade <= 9:
+        fmt.Println("Mirim")
+    case idade <= 13:
+        fmt.Println("Infantil")
+    case idade <= 17:
+        fmt.Println("Juvenil")
+    default:
+        fmt.Println("Adulto")
+    }
+    
 }
