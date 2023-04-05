ea482cd71c620cb580be598cbe4aa8b450492211
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-05 19:48:46 -0300

    Update sequencial9.go

--- sequencial9.go
+++ sequencial9.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+//Faça um algoritmo que leia o peso de uma pessoa em quilos 
+//e converta para libras.
 
+import "fmt"
+
+func main() {
+     var peso float64
+     fmt.Println("Digite seu peso (em quilos):")
+     fmt.Scan(&peso)
+     
+     
+     fmt.Println("O seu peso em libras eh :",peso*2.20462)
 }
